5c47585ea6d7a70fd7ee3e7df982aa1a2be96ff4
Ryan <rhui1@bayviewglen.ca>
2020-12-09 11:16:22 -0500

    added html info to components

--- mevnproject/src/components/NewTeam.vue
+++ mevnproject/src/components/NewTeam.vue
@@ -1,17 +1,36 @@
  <template>
- <div >
-   <div >
-     <div >
-       <div >New Team</div>
+  <div>
+    <div>
+      <h1>New Team</h1>
+    </div>
+    <form @submit.prevent="addTeam">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Team Name:</label>
+            <input type="text" class="form-control" v-model="team.name" />
+          </div>
+        </div>
+      </div>
 
-       <div >
-         This is where New teams will be inputted.
-       </div>
-     </div>
-   </div>
- </div>
- </template>
+      <br />
+      <div class="form-group">
+        <button class="btn btn-primary">Create</button>
+      </div>
+    </form>
+  </div>
+</template>
  <script>
- export default {
- }
- </script>
+export default {
+  data() {
+    return {
+      team: {},
+    };
+  },
+  methods: {
+    addTeam() {
+      console.log(this.team);
+    },
+  },
+};
+</script>
--- mevnproject/src/components/Stats.vue
+++ mevnproject/src/components/Stats.vue
@@ -5,13 +5,45 @@
        <div >Stats</div>
 
        <div >
-         This is where a table with overall statistics will be placed.
-       </div>
+         This is where a table with overall statistics will be placed.       </div>
+     <div>
+         <table class="table table-hover">
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>W</th>
+              <th>L</th>
+              <th>W/L</th>
+              <th>GB</th>
+              <th>HOME</th>
+              <th>AWAY</th>
+              <th>DIV</th>
+              <th>CONF</th>
+              <th>PPG</th>
+              <th>OPP PPG</th>
+              <th>DIFF</th>
+              <th>STRK</th>
+              <th>L10</th>
+
+            </tr>
+            </thead>
+            <tbody>
+               <!-- <tr v-for="team in teams" :key="post._id">
+                  <td>{{ post.title }}</td>
+                  <td>{{ post.body }}</td>
+                  <td>{{ post.author }}</td>
+                  <td><router-link :to="{name: 'edit', params: { id: post._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button v-on:click= deletePost(post._id) class="btn btn-danger">Delete</button>
+                  
+                  </td>
+        -->
+            </tbody>
+         </table>          
+        </div>
      </div>
    </div>
  </div>
  </template>
  <script>
- export default {
- }
- </script>
+export default {};
+</script>
