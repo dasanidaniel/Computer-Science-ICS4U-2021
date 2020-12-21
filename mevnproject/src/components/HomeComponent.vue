3ed20511b88ffed82f57ae163e4e70d99f2eee9c
Ryan <rhui1@bayviewglen.ca>
2020-12-21 16:50:10 -0500

    more text to home component

--- mevnproject/src/components/HomeComponent.vue
+++ mevnproject/src/components/HomeComponent.vue
@@ -14,9 +14,15 @@
            (e.g.) Changing a game's score and winner will change the team's statistics in the teams collection<br>
            The Team Breakdown page sorts the teams by number of wins in their conference as well as what conference they are in (East or West)<br>
            <br>
-           Apologies for the terrible CSS and style to the website<br>
-           Form over Function<br>
-           - Ryan Hui
+          
+         </p>
+         <h5>Future Changes</h5>
+           <p>In future, I would change the update, add, and delete paths for both team and game collections to instead of updating DB by every entry/action (add, delete, update), I would go through the games collection and update teams accordingly each time.
+           <br>(e.g.) adding or updating a game would trigger a method that would go through each game in the collection and reconfigure the stats of each team.
+           
+           <br>I would also fix the CSS.
+           <br>
+           <br>- Ryan Hui
          </p>
          <h3>Enjoy!</h3>
        </div>
